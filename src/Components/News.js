d3ff0a2b2a3251fa7adb81fabc1c5c3c3179e8d8
Sagar Udgiri <udgirisagar36@gmail.com>
2021-09-15 15:19:25 +0530

    Fetching News API & Pages

--- src/Components/News.js
+++ src/Components/News.js
@@ -7,16 +7,63 @@ export class News extends Component {
     constructor(props) {
         super(props);
         
-        this.state={NewsSample}
+        this.state={
+        articles:NewsSample,
+        page:1,
+        totalResults:1
+        }
         
     }
+
+    async componentDidMount(){
+
+        let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2866f892915d4382b267bb53a3f87fb9&pagesize=18";
+        let data= await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData)
+        this.setState({articles:parsedData.articles , totalResults:parsedData.totalResults})
+
+    }
+
+    handleNextClick = async () =>{
+        
+            let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2866f892915d4382b267bb53a3f87fb9&page=${this.state.page + 1}&pagesize=18`;
+            let data= await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData)
+            this.setState({
+            articles: parsedData.articles,
+            page: this.state.page + 1,
+            });
+        
+    }
+    
+    handlePrevClick = async () =>{
+
+        let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2866f892915d4382b267bb53a3f87fb9&page=${this.state.page - 1}&pagesize=18`;
+        let data= await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData)
+        this.setState({
+          articles: parsedData.articles,
+          page: this.state.page - 1,
+        });
+
+    }
+
     render() {
 
         return (
-            <div className="container my-3">
-                <h2>We are the NewsSTers</h2>
-                <NewsItem news={this.state.NewsSample}></NewsItem>
-            </div>
+            <>
+                <div className="container my-3">
+                    <h2>NewSters - Top Headlines in the US right now</h2>
+                    <NewsItem news={this.state.articles}></NewsItem>
+                </div>
+                <div className="container d-flex justify-content-between my-3">
+                    <button type="button" disabled={this.state.page<=1} className="btn btn-success" onClick={this.handlePrevClick}>&larr; Previous</button>
+                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/18)} className="btn btn-success" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </>
         )
     }
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
         const newsitems = this.props.news.map( (newz,index) => {
             return <div className="col my-3" key={index} >
                 <div className="card h-100 border border-warning shadow-0" style={{borderRadius: "20px"}}>
-                <img src={newz.urlToImage} className="card-img-top" alt={newz.title} style={{borderRadius: "20px"}}/>
+                <img src={!newz.urlToImage ? "https://www.investors.com/wp-content/uploads/2021/09/Stock-Apple-Changsha-company.jpg" : newz.urlToImage} className="card-img-top" alt={newz.title} style={{borderRadius: "20px"}}/>
                 <div className="card-body">
                     <h5 className="card-title">{newz.title}</h5>
                     <p className="card-text">{newz.description}</p>
