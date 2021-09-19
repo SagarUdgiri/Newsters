858bcbc10c9050bb06128e5eeb9e16da81027e78
Sagar Udgiri <udgirisagar36@gmail.com>
2021-09-19 15:19:38 +0530

    Adding Infinite Scroll

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ export default class App extends Component {
         <div>
         <Navbar/>
         <Switch>
-        <Route exact path="/"> <News key="general" pageSize={6} country="in" ctg="general"/></Route>
-        <Route exact path="/business"> <News key="business" pageSize={6} country="in" ctg="business"/></Route>
-        <Route exact path="/entertainment"> <News key="entertainment" pageSize={6} country="in" ctg="entertainment"/></Route>
-        <Route exact path="/general"> <News key="general" pageSize={6} country="in" ctg="general"/></Route>
-        <Route exact path="/health"> <News key="health" pageSize={6} country="in" ctg="health"/></Route>
-        <Route exact path="/science"> <News key="science" pageSize={6} country="in" ctg="science"/></Route>
-        <Route exact path="/sports"> <News key="sports" pageSize={6} country="in" ctg="sports"/></Route>
-        <Route exact path="/technology"> <News key="technology" pageSize={6} country="in" ctg="technology"/></Route>
+        <Route exact path="/"> <News key="general" pageSize={5} country="in" ctg="general"/></Route>
+        <Route exact path="/business"> <News key="business" pageSize={5} country="in" ctg="business"/></Route>
+        <Route exact path="/entertainment"> <News key="entertainment" pageSize={5} country="in" ctg="entertainment"/></Route>
+        <Route exact path="/general"> <News key="general" pageSize={5} country="in" ctg="general"/></Route>
+        <Route exact path="/health"> <News key="health" pageSize={5} country="in" ctg="health"/></Route>
+        <Route exact path="/science"> <News key="science" pageSize={5} country="in" ctg="science"/></Route>
+        <Route exact path="/sports"> <News key="sports" pageSize={5} country="in" ctg="sports"/></Route>
+        <Route exact path="/technology"> <News key="technology" pageSize={5} country="in" ctg="technology"/></Route>
         </Switch>
         </div>
       </Router>
--- src/Components/News.js
+++ src/Components/News.js
@@ -3,6 +3,8 @@ import NewsItem from './NewsItem'
 import {NewsSample} from '../newsSample'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export class News extends Component {
 
@@ -28,35 +30,42 @@ export class News extends Component {
         this.state={
         articles:NewsSample,
         page:1,
-        totalResults:1,
-        loading:false    
+        totalResults:0,
+        loading:true    
         }
         document.title=`${this.capTitle(this.props.ctg)} - NewSters`;
     }
 
     async updateNews(pageNo){
 
-        this.setState({loading:true})
         let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.ctg}&apiKey=2866f892915d4382b267bb53a3f87fb9&pagesize=${this.props.pageSize}&page=${pageNo}`;
         let data= await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData)
-        this.setState({articles:parsedData.articles , totalResults:parsedData.totalResults,loading:false})
+        this.setState({articles:this.state.articles.concat(parsedData.articles) , totalResults:parsedData.totalResults,loading:false})
     }
 
     async componentDidMount(){
+        
+        this.setState({articles:[]})
         this.updateNews(1);
     }
 
-    handleNextClick = async () =>{
-        this.updateNews(this.state.page + 1)
-        this.setState({page: this.state.page + 1});
-    }
+    fetchMoreData = () => {
+
+        this.setState({page:this.state.page + 1})
+        this.updateNews(this.state.page)
+      };
+
+    // handleNextClick = async () =>{
+    //     this.updateNews(this.state.page + 1)
+    //     this.setState({page: this.state.page + 1});
+    // }
     
-    handlePrevClick = async () =>{
-        this.updateNews(this.state.page - 1)
-        this.setState({page: this.state.page - 1});
-    }
+    // handlePrevClick = async () =>{
+    //     this.updateNews(this.state.page - 1)
+    //     this.setState({page: this.state.page - 1});
+    // }
 
     render() {
 
@@ -64,13 +73,23 @@ export class News extends Component {
             <>
                 <div className="container my-3">
                     <h2 className="text-center my-5">NewSters - Top {this.capTitle(this.props.ctg)} Headlines</h2>
-                    {this.state.loading && <Spinner/>}
-                    {!this.state.loading && <NewsItem news={this.state.articles}></NewsItem>}
+                    {/* {this.state.loading && <Spinner/>} */}
+                        <InfiniteScroll
+                                        dataLength={this.state.articles.length}
+                                        next={this.fetchMoreData}
+                                        hasMore={this.state.articles.length !== this.state.totalResults}
+                                        loader={<Spinner/>}
+                                        >
+                    <div className="container">
+                    <NewsItem news={this.state.articles}></NewsItem>
+                    </div>
+                    
+                    </InfiniteScroll>
                 </div>
-                <div className="container d-flex justify-content-between my-3">
+                {/* <div className="container d-flex justify-content-between my-3">
                     <button type="button" disabled={this.state.page<=1} className="btn btn-success" onClick={this.handlePrevClick}>&larr; Previous</button>
                     <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-success" onClick={this.handleNextClick}>Next &rarr;</button>
-                </div>
+                </div> */}
             </>
         )
     }
